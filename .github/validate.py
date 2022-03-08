e1fc065b595d2e460b4d9e038fc8b52d5bff26e6
Eric Lyons <53839634+eric-lyons@users.noreply.github.com>
2022-03-08 17:48:59 -0500

    Update validate.py

--- .github/validate.py
+++ .github/validate.py
@@ -26,7 +26,7 @@ def initialize(dev):
     RETURNS: SDK session/token
     """
 
-    sdk = looker_sdk.init40()
+    sdk = looker_sdk.init40(base_url="https://master.dev.looker.com:19999")
     if dev == "True":
         sdk.update_session(body=models40.WriteApiSession(workspace_id="dev"))
 
